bed6bd5cffc23e92040bf597a17bf73a49fd938e
Mauricio <zub_gg@hotmail.com>
2025-12-09 22:03:27 -0300

    desplegable del navbar actualizado

--- src/components/Desplegable.tsx
+++ src/components/Desplegable.tsx
@@ -40,25 +40,24 @@ export default function Desplegable() {
                     initial={{ opacity: 0, height: 0 }}
                     animate={{ opacity: 1, height: "auto" }}
                     exit={{ opacity: 0, height: 0 }}
-                    transition={{ duration: 0.05 }}
+                    transition={{ duration: 0.01 }}
                     style={{
                         position: "absolute",
                         top: "100%",
-                        left: 0,
+                        right: '-5.3rem',
                         zIndex: 10,
-                        width: "11rem",
+                        width: "12.5rem",
                         background: "#025159",
-                        borderRadius: "0 0 8px 8px",
                         boxShadow: "0 4px 16px rgba(0,0,0,0.12)",
-                        padding: "1vh",
                         display: "flex",
                         flexDirection: "column",
+                        transform: "skew(40deg)",
                         justifyContent: "center",
-                        alignItems: "center"
+                        alignItems: "center",
                     }}
                 >
-                    <ul className="btn-desplegable-abierto" style={{ width: "100%", textAlign: "center", margin: 0, padding: 0 }}>
-                        <li className="mb-2 text-body-desplegable" style={{ width: "100%" }}>
+                    <ul className="btn-desplegable-abierto" style={{ width: "100%", textAlign: "center", margin: 0, padding: 0, paddingBottom:"10px" }}>
+                        <li className="py-[.5rem] text-body-desplegable" style={{ width: "100%" ,transition:".5s"}}>
                             <ScrollLink
                                 to="sobre-folkode"
                                 spy={true}
@@ -71,7 +70,7 @@ export default function Desplegable() {
                                 Nosotros
                             </ScrollLink>
                         </li>
-                        <li className="mb-2 text-body-desplegable" style={{ width: "100%" }}>
+                        <li className="py-[.5rem] text-body-desplegable" style={{ width: "100%" ,transition:".5s"}}>
                             <ScrollLink
                                 to="nuestro-equipo"
                                 spy={true}
@@ -84,7 +83,7 @@ export default function Desplegable() {
                                 Nuestro Equipo
                             </ScrollLink>
                         </li>
-                        <li className="mb-2 text-body-desplegable" style={{ width: "100%" }}>
+                        <li className="py-[.5rem] text-body-desplegable" style={{ width: "100%" ,transition:".5s"}}>
                             <ScrollLink
                                 to="que-ofrecemos"
                                 spy={true}
